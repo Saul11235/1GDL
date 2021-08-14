76a8deee6443943ef0c8fbb599946eca80fc9794
Edwin Saul <edwinsaulpm@gmail.com>
2021-08-13 21:00:46 -0500

    creada ClaseAnalisis.py

---
+++ ClaseAnalisis.py
@@ -0,0 +1,10 @@
+#esta es una clase que abstrae el analisis con sus datos
+class Analisis:
+    def __init__(self,NombreAnalisis):
+        self.NombreAnalisis=NombreAnalisis
+        self.Datos={}
+
+if __name__="__main__":
+    a=Analisis()
+    print("test1")
+
